5e7acc67a02aa12f61538fb33caf2c5d60cfc14e
keepsimple <75323481+keeepsimple@users.noreply.github.com>
2024-09-08 11:38:22 +0700

    fix error redis

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,15 @@
-import { CacheInterceptor, CacheModule } from '@nestjs/cache-manager';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { APP_INTERCEPTOR } from '@nestjs/core';
 import { AuthorModule } from './author/author.module';
 import { CategoryModule } from './category/category.module';
 import { ChapterModule } from './chapter/chapter.module';
 import { ComicModule } from './comic/comic.module';
 import { CommentModule } from './comment/comment.module';
-import { RedisConfig } from './config/redis.config';
 import { DatabaseModule } from './database/database.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
-    CacheModule.registerAsync(RedisConfig),
     CategoryModule,
     DatabaseModule,
     ComicModule,
@@ -23,10 +19,10 @@ import { DatabaseModule } from './database/database.module';
   ],
   controllers: [],
   providers: [
-    {
-      provide: APP_INTERCEPTOR,
-      useClass: CacheInterceptor,
-    },
+    // {
+    //   provide: APP_INTERCEPTOR,
+    //   useClass: CacheInterceptor,
+    // },
   ],
 })
 export class AppModule {}
